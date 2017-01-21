73452b011b0bb8e2f3ded77e7772a6ab24be2f2c
xoakzxz <juandavid_ja@hotmail.com>
2017-01-21 07:05:12 -0500

    Yolo

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour {
+
+    public int[] score = new int[3]; //The score[0] is the Min and the score[2] is the Max
+    public string[] scoreName = new string[3];
+
+    [SerializeField]
+    private Text[] scores = new Text[3];
+
+    //Unity functions
+
+    void Start()
+    {
+        SetText();
+    }
+
+    //Other functions
+
+    public void CompareScore (string _scoreName, int _score)
+    {
+        if (_score > score[0])
+        {
+            if (_score>score[1])
+            {
+                if(_score>score[2])
+                {
+                    score[0] = score[1];
+                    scoreName[0] = scoreName[1];
+                    score[1] = score[2];
+                    scoreName[1] = scoreName[2];
+                    score[2] = _score;
+                    scoreName[2] = _scoreName;
+
+                    //Playerprefs
+
+                    PlayerPrefs.SetInt("score0", score[1]);
+                    PlayerPrefs.SetString("scoreName0", scoreName[1]);
+                    PlayerPrefs.SetInt("score1", score[2]);
+                    PlayerPrefs.SetString("scoreName1", scoreName[2]);
+                    PlayerPrefs.SetInt("score2", _score);
+                    PlayerPrefs.SetString("scoreName2", _scoreName);
+
+                    SetText();
+                }
+                else
+                {
+                    score[0] = score[1];
+                    scoreName[0] = scoreName[1];
+                    score[1] = _score;
+                    scoreName[1] = _scoreName;
+
+                    //Playerprefs
+
+                    PlayerPrefs.SetInt("score0", score[1]);
+                    PlayerPrefs.SetString("scoreName0", scoreName[1]);
+                    PlayerPrefs.SetInt("score1", _score);
+                    PlayerPrefs.SetString("scoreName1", _scoreName);
+
+                    SetText();
+                }
+            }
+            else
+            {
+                score[0] = _score;
+                scoreName[0] = _scoreName;
+
+                //Playerprefs
+
+                PlayerPrefs.SetInt("score0", _score);
+                PlayerPrefs.SetString("scoreName0", _scoreName);
+
+                SetText();
+            }
+        }
+        else
+        {
+            Debug.Log("Sorry :(");
+        }
+    }
+
+    private void SetText()
+    {
+        int position = 1;
+        int score = 0;
+
+        foreach (Text text in scores)
+        {
+            text.text = position + ". " + PlayerPrefs.GetString("scoreName" + score.ToString()) + ": " + PlayerPrefs.GetInt("score" + score.ToString());
+            position++;
+            score++;
+        }
+    }
+}
